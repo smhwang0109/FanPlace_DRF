2a58eb36a3f2ef115b098d4e0d594ad8c2f2b60b
Soomin Hwang <smhwang0109@gmail.com>
2020-06-15 14:47:33 +0900

    SerializerMethodField 정리

--- articles/serializers.py
+++ articles/serializers.py
@@ -5,17 +5,6 @@ from accounts.serializers import UserSerializer
 from actors.serializers import ActorSerializer
 from .models import Article, ArticleComment, ArticleLike
 
-import json
-from django.core.serializers.json import DjangoJSONEncoder
-from django.db.models.query import QuerySet
-from django.http import JsonResponse
-
-class MyJSONEncoder(DjangoJSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, QuerySet):
-            return dict(obj)
-        return super().default(obj)
-
 class ArticleSerializer(serializers.ModelSerializer):
     user = UserSerializer(required=False)
     actor = ActorSerializer(required=False)
@@ -27,9 +16,7 @@ class ArticleSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def get_comments(self, obj):
-        # return list(obj.comments.all())
-        return django_serializers.serialize('json', obj.comments.all(), cls=MyJSONEncoder, ensure_ascii=False)
-        # return json.dumps(list(obj.comments.all().values()), )
+        return django_serializers.serialize('json', obj.comments.all(), ensure_ascii=False)
 
 class ArticleCommentSerializer(serializers.ModelSerializer):
     user = UserSerializer(required=False)
