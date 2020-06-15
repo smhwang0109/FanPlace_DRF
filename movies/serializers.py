7dc67e635cc3007af4226757e7a29cca97a9ae59
Soomin Hwang <smhwang0109@gmail.com>
2020-06-15 20:15:40 +0900

    ReviewSerializers에 comments 추가

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,3 +1,5 @@
+from django.core import serializers as django_serializers
+
 from rest_framework import serializers
 from accounts.serializers import UserSerializer
 from .models import Movie, Review, ReviewComment
@@ -10,9 +12,15 @@ class MovieSerializer(serializers.ModelSerializer):
 class ReviewSerializer(serializers.ModelSerializer):
     user = UserSerializer(required=False)
     movie = MovieSerializer(required=False)
+    
+    comments = serializers.SerializerMethodField()
+
     class Meta:
         model = Review
         fields = '__all__'
+
+    def get_comments(self, obj):
+        return django_serializers.serialize('json', obj.comments.all(), ensure_ascii=False)
 
 class ReviewCommentSerializer(serializers.ModelSerializer):
     user = UserSerializer(required=False)
